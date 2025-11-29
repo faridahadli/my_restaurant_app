6ebec1188f18a9009e56dedf222665e1c77fbdb3
Farid Ahadli <farid.ahadli@outlook.com>
2025-11-29 23:29:38 +0400

    Develop Entities add fields/methods to existing entity classes and create new ones. Create Cart class. Not sure how to implement a map of menu items yet. Change dadtabase from MySql to H2

--- src/main/java/com/farid/ahadli/my_restaurant_app/controller/MyController.java
+++ src/main/java/com/farid/ahadli/my_restaurant_app/controller/MyController.java
@@ -1,9 +1,10 @@
 package com.farid.ahadli.my_restaurant_app.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/")
 public class MyController {
 
 }
