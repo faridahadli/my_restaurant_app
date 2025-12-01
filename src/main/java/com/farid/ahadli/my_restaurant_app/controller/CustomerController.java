28cb9b2d90204985093734c61383847b4dd4316a
Farid Ahadli <farid.ahadli@outlook.com>
2025-12-01 10:51:54 +0400

    FINIS CUSTOMER CONTROLLER
    Customer controller is mostly finished

---
+++ src/main/java/com/farid/ahadli/my_restaurant_app/controller/CustomerController.java
@@ -0,0 +1,70 @@
+package com.farid.ahadli.my_restaurant_app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/")
+public class CustomerController {
+    @GetMapping("/cart/items")
+    public ResponseEntity<?>  getAllCartItems(){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @GetMapping("/cart/items/{id}")
+    public ResponseEntity<?>  getCartItemById(@PathVariable int id){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @PostMapping("/cart/items/")
+    public ResponseEntity<?>  addCartItem(@RequestBody Integer id,@RequestBody Integer quantity){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @DeleteMapping("/cart/items/{id}")
+    public ResponseEntity<?>  deleteCartItem(@PathVariable Integer id){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @PutMapping("/cart/items/{id}")
+    public ResponseEntity<?>  updateCartItem(@PathVariable Integer id, @RequestBody Integer quantity){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+    @DeleteMapping("/cart/items/")
+    public ResponseEntity<?>  deleteAllCartItems(@RequestBody Integer id){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<?>  createOrder(){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+    @DeleteMapping("/order")
+    public ResponseEntity<?>  cancelOrder(){
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(null);
+    }
+
+
+
+
+}
